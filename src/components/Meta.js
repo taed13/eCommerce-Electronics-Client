481266b5471dc02f23c7849d1a57542e35f1af6e
Tấn Cuỳnh <binphan120302@gmail.com>
2024-04-19 17:26:40 +0700

    #3/4:38:00/create product list

---
+++ src/components/Meta.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = (props) => {
+    return (
+        <Helmet>
+            <meta charSet='uft-8' />
+            <title>{props.title}</title>
+        </Helmet>
+    )
+}
+
+export default Meta
