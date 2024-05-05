2c78e1f1d615fc5482f27dc3e434b189e53135af
taed13 <taedtech13@gmail.com>
2024-05-05 20:59:32 +0700

    done login signup upto vid20

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,40 +1,90 @@
-import React from 'react'
-import Meta from '../components/Meta'
-import BreadCrumb from '../components/BreadCrumb'
-import { Link } from 'react-router-dom'
-import Container from '../components/Container'
+import React from "react";
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
+import { Link } from "react-router-dom";
+import Container from "../components/Container";
+import CustomInput from "../components/CustomInput";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../features/user/userSlice";
+
+const loginSchema = yup.object({
+  email: yup
+    .string()
+    .email("Email should be valid")
+    .required("Email is required"),
+  password: yup.string().required("Password is required"),
+});
 
 const Login = () => {
-    return (
-        <>
-            <Meta title={"Login"} />
-            <BreadCrumb title="Login" />
-            <Container class1="login-wrapper py-5 home-wrapper-2">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="auth-card">
-                            <h3 className='text-center mb-3'>Login</h3>
-                            <form action="" className='d-flex flex-column gap-15'>
-                                <div>
-                                    <input type="email" name='email' placeholder='Email' className="form-control" />
-                                </div>
-                                <div className='mt-1'>
-                                    <input type="password" name='password' placeholder='Password' className="form-control" />
-                                </div>
-                                <div>
-                                    <Link to='/forgot-password'>Forgot password?</Link>
-                                </div>
-                                <div className='mt-3 d-flex justify-content-center align-items-center gap-15'>
-                                    <button type='submit' className='button border-0'>Login</button>
-                                    <Link to='/signup' className='button signup'>Signup</Link>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: loginSchema,
+    onSubmit: (values) => {
+      dispatch(loginUser(values));
+    },
+  });
+  return (
+    <>
+      <Meta title={"Login"} />
+      <BreadCrumb title="Login" />
+      <Container class1="login-wrapper py-5 home-wrapper-2">
+        <div className="row">
+          <div className="col-12">
+            <div className="auth-card">
+              <h3 className="text-center mb-3">Login</h3>
+              <form
+                action=""
+                onSubmit={formik.handleSubmit}
+                className="d-flex flex-column gap-15"
+              >
+                <CustomInput
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange("email")}
+                  onBlur={formik.handleBlur("email")}
+                />
+                <div className="error text-danger">
+                  {formik.touched.email && formik.errors.email}
                 </div>
-            </Container>
-        </>
-    )
-}
 
-export default Login
+                <CustomInput
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange("password")}
+                  onBlur={formik.handleBlur("password")}
+                />
+                <div className="error text-danger">
+                  {formik.touched.password && formik.errors.password}
+                </div>
+
+                <div>
+                  <Link to="/forgot-password">Forgot password?</Link>
+                </div>
+                <div className="mt-3 d-flex justify-content-center align-items-center gap-15">
+                  <button type="submit" className="button border-0">
+                    Login
+                  </button>
+                  <Link to="/signup" className="button signup">
+                    Signup
+                  </Link>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,36 +1,42 @@
-import React from 'react'
-import Meta from '../components/Meta'
-import BreadCrumb from '../components/BreadCrumb'
-import { Link } from 'react-router-dom'
-import Container from '../components/Container'
+import React from "react";
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
+import Container from "../components/Container";
+import CustomInput from "../components/CustomInput";
 
 const ResetPassword = () => {
-    return (
-        <>
-            <Meta title={"Reset Password"} />
-            <BreadCrumb title="Reset Password" />
-            <Container class1="login-wrapper py-5 home-wrapper-2">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="auth-card">
-                            <h3 className='text-center mb-3'>Reset Password</h3>
-                            <form action="" className='d-flex flex-column gap-15'>
-                                <div>
-                                    <input type="password" name='password' placeholder='Password' className="form-control" />
-                                </div>
-                                <div>
-                                    <input type="password" name='confpassword' placeholder='Confirm password' className="form-control" />
-                                </div>
-                                <div className='mt-3 d-flex justify-content-center flex-column align-items-center gap-15'>
-                                    <button className='button border-0'>Reset password</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </Container>
-        </>
-    )
-}
+  return (
+    <>
+      <Meta title={"Reset Password"} />
+      <BreadCrumb title="Reset Password" />
+      <Container class1="login-wrapper py-5 home-wrapper-2">
+        <div className="row">
+          <div className="col-12">
+            <div className="auth-card">
+              <h3 className="text-center mb-3">Reset Password</h3>
+              <form action="" className="d-flex flex-column gap-15">
+                <CustomInput
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                />
 
-export default ResetPassword
+                <CustomInput
+                  type="password"
+                  name="confpassword"
+                  placeholder="Confirm password"
+                />
+
+                <div className="mt-3 d-flex justify-content-center flex-column align-items-center gap-15">
+                  <button className="button border-0">Reset password</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default ResetPassword;
